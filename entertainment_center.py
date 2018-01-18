27c0ca537ccf792af5885add07eb2360097a99e3
Mario Fink <mario.fink@goodyear.com>
2018-01-18 10:41:42 +0100

    create list of favourite movies

---
+++ entertainment_center.py
@@ -0,0 +1,13 @@
+from media import Movie
+
+movielist = [
+    Movie("Scott Pilgrim Vs. The World",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkwNTczNTMyOF5BMl5BanBnXkFtZTcwNzUxOTUyMw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+          "https://www.youtube.com/watch?v=7wd5KEaOtm4"),
+    Movie("Kung Fu Hustle",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BYmNhNzE2YjEtMDNmNS00ZGNkLTljZjktYTdlNGNmYzBmNjIyL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
+          "https://www.youtube.com/watch?v=-m3IB7N_PRk"),
+    Movie("Star Wars",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg",
+          "https://www.youtube.com/watch?v=vZ734NWnAHA")
+]
