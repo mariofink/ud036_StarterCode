c902f7c2c256798cfb11dcb9edecfb8ecf7fa555
Mario Fink <mario.fink@goodyear.com>
2018-01-18 15:31:07 +0100

    add comments and noqa markers according to code review

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,13 +1,14 @@
 from media import Movie
 
+""" List of movie objects to be rendered by fresh_tomatoes.py """
 movielist = [
     Movie("Scott Pilgrim Vs. The World",
-          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkwNTczNTMyOF5BMl5BanBnXkFtZTcwNzUxOTUyMw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkwNTczNTMyOF5BMl5BanBnXkFtZTcwNzUxOTUyMw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",  # noqa
           "https://www.youtube.com/watch?v=7wd5KEaOtm4"),
     Movie("Kung Fu Hustle",
-          "https://images-na.ssl-images-amazon.com/images/M/MV5BYmNhNzE2YjEtMDNmNS00ZGNkLTljZjktYTdlNGNmYzBmNjIyL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BYmNhNzE2YjEtMDNmNS00ZGNkLTljZjktYTdlNGNmYzBmNjIyL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",  # noqa
           "https://www.youtube.com/watch?v=-m3IB7N_PRk"),
     Movie("Star Wars",
-          "https://images-na.ssl-images-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg",  # noqa
           "https://www.youtube.com/watch?v=vZ734NWnAHA")
 ]
--- media.py
+++ media.py
@@ -1,7 +1,12 @@
 class Movie:
-    """ Stores movie information """
+    """Stores movie information """
 
     def __init__(self, title, poster, trailer_youtube_url):
+        """Store the following information for a movie object
+        * title
+        * an url of an image of the movie’s poster
+        * an url linking to a movie trailer on YouTube
+        """
         self.title = title
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer_youtube_url
