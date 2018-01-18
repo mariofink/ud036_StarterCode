84dddb0644846ff4627a2c01fb137f4b7beae68b
Mario Fink <mario.fink@goodyear.com>
2018-01-18 10:41:24 +0100

    add class Movie

---
+++ media.py
@@ -0,0 +1,7 @@
+class Movie:
+    """ Stores movie information """
+
+    def __init__(self, title, poster, trailer_youtube_url):
+        self.title = title
+        self.poster_image_url = poster
+        self.trailer_youtube_url = trailer_youtube_url
